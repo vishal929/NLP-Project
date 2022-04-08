07d30f322bc63a66e9702a573fdbd5d603511d33
vishal929 <vishal.patel929@gmail.com>
2022-04-08 17:13:41 -0400

    added FID , IS files

---
+++ Metrics/IS.py
@@ -0,0 +1,6 @@
+# calculating Inception Score for GANs
+
+# idea is to compute KL divergence between image label distribution and the generated marginal distribution
+
+# IS = exp(Expected_x KL(p(y|x) || p(y)) )
+
