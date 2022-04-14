df08b308b9d3ccbb2cdf0816710904981e63bb66
vishal929 <vishal.patel929@gmail.com>
2022-04-14 19:50:04 -0400

    starting to write some logic

--- Modules/AttentionalBlock.py
+++ Modules/AttentionalBlock.py
@@ -9,6 +9,10 @@ class AttentionalBlock(torch.nn.Module):
     # # attentional block
     def __init__(self,in_channel, out_channel, predict_mask=True):
         super.__init__()
+        # need a convolution to project the input channel space to the output channel space if they are not the same
+        self.dimExpansionNeeded = in_channel != out_channel
+        self.predict_mask = predict_mask
+        self.firstConvolution = None
 
     # x is the image channel data
     # y is the text encoding
