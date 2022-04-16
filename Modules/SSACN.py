0313fd358f60dbbce4a31b08c144864adadf9920
micyen <tacosheeba@gmail.com>
2022-04-16 17:42:51 -0400

    i think i fixed a little bit

--- Modules/SSACN.py
+++ Modules/SSACN.py
@@ -4,7 +4,6 @@ import torch
 import torch.nn as nn
 import numpy as np
 import torch.nn.functional as f
-# from sync_batchnorm import SynchronizedBatchNorm2d
 
 # Block that takes text feature vector and image feature maps from last SSACN block as input
 # Outputs new image feature maps
@@ -14,14 +13,13 @@ class SSACNBlock(nn.Module):
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.upsample = f.interpolate()
         self.mask_predictor = MaskPredictor(self.out_channels)
         self.sscbn = SSCBN(self.in_channels)
 
     def forward(self, image_features, text_features, mask=None):
-        image_features = self.upsample(image_features)
-        mask = self.mask_predictor(image_features)
-        output_image_features = self.sscbn(image_features, text_features, mask)
+        upscaled_image_features = f.interpolate(image_features, scale_factor=2)
+        mask = self.mask_predictor(upscaled_image_features)
+        output_image_features = self.sscbn(upscaled_image_features, text_features, mask)
 
         return output_image_features
 
@@ -29,19 +27,17 @@ class SSACNBlock(nn.Module):
 class MaskPredictor(nn.Module):
     def __init__(self, out_ch):
         super().__init__()
-        self.conv1 = nn.Conv2D(out_ch, 100, 3, 1, 1)
-        # TODO: replace this batchnorm with SSCBN
+        self.conv1 = nn.Conv2d(out_ch, 100, 3, 1, 1)
         self.batchnorm = nn.BatchNorm2d(100) 
         self.relu = nn.ReLU()
-        self.conv2 = nn.Conv2D(100, 1, 1, 1, 0)
-        self.sigmoid = nn.sigmoid()
+        self.conv2 = nn.Conv2d(100, 1, 1, 1, 0)
 
     def forward(self, upsampled_features):
         mask = self.conv1(upsampled_features)
         mask = self.batchnorm(mask)
         mask = self.relu(mask)
         mask = self.conv2(mask)
-        mask = self.sigmoid(mask)
+        mask = torch.sigmoid(mask)
 
         return(mask)
 
@@ -66,16 +62,16 @@ class SSCBN(nn.Module):
         nn.init.zeros_(beta_linear2.weight.data)
         nn.init.zeros_(beta_linear2.bias.data)
 
-        self.fc_gamma = nn.Sequential([
+        self.fc_gamma = nn.Sequential(
             gamma_linear1,
             gamma_relu,
             gamma_linear2
-        ])
-        self.fc_beta = nn.Sequential([
+        )
+        self.fc_beta = nn.Sequential(
             beta_linear1,
             beta_relu,
             beta_linear2
-        ])
+        )
 
     def forward(self, upsampled_image_features, text_features, predicted_mask):
         bn_upsampled_image_features = self.batch_norm2d(upsampled_image_features)
