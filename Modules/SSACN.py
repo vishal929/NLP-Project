95d6a798ee644e4bbc9e02c94f769e24d885f4c1
micyen <tacosheeba@gmail.com>
2022-04-18 17:27:40 -0400

    minor changes

--- Modules/SSACN.py
+++ Modules/SSACN.py
@@ -5,22 +5,27 @@ import torch.nn as nn
 import numpy as np
 import torch.nn.functional as f
 
+# TODO: Correct Input / output channels
+# TODO: learnable shortcut if the input and output channels don't match
+# TODO: look at learnable_sc
 # Block that takes text feature vector and image feature maps from last SSACN block as input
 # Outputs new image feature maps
 # Each SSACN has an upsample block, a mask predictor, a SSCBN and a residual block
+# batch size, height, width, channels
 class SSACNBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, predict_mask=True):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
+        self.learnable_sc = (in_channels != out_channels)
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.mask_predictor = MaskPredictor(self.out_channels)
-        self.sscbn = SSCBN(self.in_channels)
+        self.sscbn = SSCBN(self.out_channels, self.learnable_sc)
 
+# check scale_factor
     def forward(self, image_features, text_features, mask=None):
-        upscaled_image_features = f.interpolate(image_features, scale_factor=2)
-        # h, w = upscaled_image_features.size(2), upscaled_image_features.size(3)
-        # upscaled_image_features = f.interpolate(image_features, size=(h*2, w*2, ch), mode='bilinear', align_corners=True)
-        # stage_mask = F.interpolate(stage_mask, size=(hh, ww), mode='bilinear', align_corners=True)
+        print(image_features.shape)
+        upscaled_image_features = f.interpolate(image_features, scale_factor=self.out_channels/self.in_channels)
+        print(upscaled_image_features.shape)
         mask = self.mask_predictor(upscaled_image_features)
         output_image_features = self.sscbn(upscaled_image_features, text_features, mask)
 
@@ -36,19 +41,25 @@ class MaskPredictor(nn.Module):
         self.conv2 = nn.Conv2d(100, 1, 1, 1, 0)
 
     def forward(self, upsampled_features):
+        print(upsampled_features.shape)
         mask = self.conv1(upsampled_features)
+        print(mask.shape)
         mask = self.batchnorm(mask)
+        print(mask.shape)
         mask = self.relu(mask)
+        print(mask.shape)
         mask = self.conv2(mask)
+        print(mask.shape)
         mask = torch.sigmoid(mask)
 
         return(mask)
 
 # SSCBN used in SSACN (includes residual connection)
 class SSCBN(nn.Module):
-    def __init__(self, num_features_output):
+    def __init__(self, num_features_output, learnable_sc):
         super().__init__()
 
+        # self.learnable_sc = learnable_sc
         self.batch_norm2d = nn.BatchNorm2d(num_features_output, affine=False)
 
         gamma_linear1 = nn.Linear(256, 256)
@@ -76,9 +87,19 @@ class SSCBN(nn.Module):
             beta_linear2
         )
 
+        # if self.learnable_sc:
+        # self.conv_sc = nn.Conv2d(in_ch, out_ch, 1, stride=1, padding=0)
+
     def forward(self, upsampled_image_features, text_features, predicted_mask):
         bn_upsampled_image_features = self.batch_norm2d(upsampled_image_features)
         weight = self.fc_gamma(text_features)
         bias = self.fc_beta(text_features)
 
-        return predicted_mask * (weight * bn_upsampled_image_features + bias) + bn_upsampled_image_features
+        print(predicted_mask.shape)
+        print(bn_upsampled_image_features.shape)
+        print(weight.shape)
+        print(bias.shape)
+
+        sscbn = predicted_mask * (weight * bn_upsampled_image_features + bias) + bn_upsampled_image_features
+
+        return sscbn
