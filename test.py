88cafbc74f7aedcb8815ed2dd8680106e9c38502
vishal929 <vishal.patel929@gmail.com>
2022-05-04 02:26:12 -0400

    grabbed all test images successfully

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import torch
 import os
 from collections import OrderedDict
 
+from PIL import Image
 from torch.utils.data import DataLoader
 import torchvision
 from torch.utils.tensorboard import SummaryWriter
@@ -14,14 +15,38 @@ from Modules.Generator import Generator
 from TextEncoder.RNNEncoder import bilstmEncoder
 from ImageEncoder.CNNEncoder import cnnEncoder
 from tqdm import tqdm
+import random
+import numpy as np
+
+def textFromIndices(indexCaption, captionLength, indexToWord):
+    indexCaption = indexCaption[:captionLength]
+    textCaption = ""
+    for index in indexCaption:
+        textCaption += " " + indexToWord[index.item()]
+    return textCaption
 
 # dont need discriminator or image encoder for testing, we are just saving generated images basically
-def test(dataloader, generator, textEncoder, device, batch_size):
+# need the indexToWord dictionary to build word from caption
+def test(dataloader, indexToWord, generator, textEncoder, device, batch_size):
+
+    fakeDir = os.path.join(os.getcwd(),'test_fake')
+    captionDir = os.path.join(os.getcwd(),'test_captions')
+
+    '''
+    realDir = os.path.join(os.getcwd(),'test_real')
+
+    if not os.path.isdir(realDir):
+        os.mkdir(realDir)
+    '''
+
+    if not os.path.isdir(fakeDir):
+        os.mkdir(fakeDir)
+
+    if not os.path.isdir(captionDir):
+        os.mkdir(captionDir)
 
     # testing, or "saving" generated images
-
-    # summary writer for keeping track of testing
-    writer = SummaryWriter('testStuff')
+    # then, we will evaluate FID and IS scores on the saved images
     dataIterator = iter(dataloader)
     for step in tqdm(range(len(dataIterator))):
         # dont need gradients for testing
@@ -60,24 +85,62 @@ def test(dataloader, generator, textEncoder, device, batch_size):
             x_fake = generator(z, sentEmbeddings)
 
             # we need to save the real image associated with the generated image for FID score
-            # saving generatedImage, realImage, and associated captions
+            # saving generatedImage, realImage, and associated captions as the following name space:
+            # realImage: "test_real/INDEX.jpeg"
+            # generatedImage: "test_fake/INDEX.jpeg"
+            # associated caption: "test_caption/INDEX.txt"
+
+            # transform after tanh for every pixel value for every image in the batch
+            x_fake = (x_fake + 1.0) * 127.5
+            for index, img in enumerate(x_fake):
+                img = img.cpu().numpy()
+                img = img.astype(np.uint8)
+                img = np.transpose(img,(1,2,0))
+                img = Image.fromarray(img)
+                saveName = str((step * x_fake.shape[0])+ index) + '.png'
+                img.save(os.path.join(fakeDir,saveName))
+
+
+            # saving the captions
+            testCaptions = testCaptions.cpu()
+            captionLengths = captionLengths.cpu()
+            for index, caption in enumerate(testCaptions):
+                captionLength = captionLengths[index]
+                caption = textFromIndices(caption,captionLength, indexToWord)
+                saveName = str((step * x_fake.shape[0])+ index) + '.txt'
+                with open(os.path.join(captionDir,saveName),'w') as f:
+                    f.write(caption)
+
+
+
+
 
             #img = trainImages[0].to(device)
 
             #write_images_losses(writer, img, x_fake, L_advD, L_advD, genLoss, finalLoss, epoch)
 
 if __name__ == '__main__':
+    # transform for images
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(int(256 * 76 / 64)),
+        torchvision.transforms.RandomCrop(256),
+        torchvision.transforms.RandomHorizontalFlip()])
+
+    # setting seed since we are evaluating
+    random.seed(100)
+    np.random.seed(100)
+    torch.manual_seed(100)
     # grab dataset
-    cubImageDir = os.path.join(os.getcwd(),'..','CUBS Dataset','Cubs-2011','cub-200-2011-20220408T185459Z-001',
+    cubImageDir = os.path.join(os.getcwd(),'CUBS Dataset','Cubs-2011','cub-200-2011-20220408T185459Z-001',
                                'cub-200-2011','CUB_200_2011','CUB_200_2011','images')
     '''
     cubImageDir = '../CUBS Dataset/Cubs-2011/cub-200-2011-20220408T185459Z-001/cub-200-2011/CUB_200_2011/CUB_200_2011/' \
                   'images'
     '''
-    cubCaptionDir = os.path.join(os.getcwd(),'..','CUBS Dataset','Cubs-2011','bird_metadata','birds','text','text')
+    cubCaptionDir = os.path.join(os.getcwd(),'CUBS Dataset','Cubs-2011','bird_metadata','birds','text','text')
 
     # place where cub metadata is
-    cubBoundingBoxFile = os.path.join(os.getcwd(),'..','CUBS Dataset','Cubs-2011','cub-200-2011-20220408T185459Z-001',
+    cubBoundingBoxFile = os.path.join(os.getcwd(),'CUBS Dataset','Cubs-2011','cub-200-2011-20220408T185459Z-001',
                                'cub-200-2011','CUB_200_2011','CUB_200_2011','bounding_boxes.txt')
     '''
     cubBoundingBoxFile = './CUBS Dataset/Cubs-2011/cub-200-2011-20220408T185459Z-001/cub-200-2011/' \
@@ -87,17 +150,17 @@ if __name__ == '__main__':
     cubFileMappings = './CUBS Dataset/Cubs-2011/cub-200-2011-20220408T185459Z-001/cub-200-2011/' \
                       'CUB_200_2011/CUB_200_2011/images.txt'
     '''
-    cubFileMappings = os.path.join(os.getcwd(), '..', 'CUBS Dataset', 'Cubs-2011',
+    cubFileMappings = os.path.join(os.getcwd(), 'CUBS Dataset', 'Cubs-2011',
                                       'cub-200-2011-20220408T185459Z-001',
                                       'cub-200-2011', 'CUB_200_2011', 'CUB_200_2011', 'images.txt')
-    cubClasses = os.path.join(os.getcwd(), '..', 'CUBS Dataset', 'Cubs-2011',
+    cubClasses = os.path.join(os.getcwd(), 'CUBS Dataset', 'Cubs-2011',
                                    'cub-200-2011-20220408T185459Z-001',
                                    'cub-200-2011', 'CUB_200_2011', 'CUB_200_2011', 'image_class_labels.txt')
     '''
     cubClasses = './CUBS Dataset/Cubs-2011/cub-200-2011-20220408T185459Z-001/cub-200-2011/' \
                  './CUB_200_2011/image_class_labels.txt'
     '''
-    cubTrainTestSplit = os.path.join(os.getcwd(), '..', 'CUBS Dataset', 'Cubs-2011',
+    cubTrainTestSplit = os.path.join(os.getcwd(), 'CUBS Dataset', 'Cubs-2011',
                               'cub-200-2011-20220408T185459Z-001',
                               'cub-200-2011', 'CUB_200_2011', 'CUB_200_2011', 'train_test_split.txt')
     '''
@@ -105,15 +168,18 @@ if __name__ == '__main__':
                         'CUB_200_2011/CUB_200_2011/train_test_split.txt'
     '''
 
-    pickleDir = os.path.join(os.getcwd(),'CUBMetadata')
+    pickleDir = os.path.join(os.getcwd(),'DatasetPreparation','CUBMetadata')
 
     # setting up dataset if not setup already (i.e pickles dont exist)
-    prepData.setupCUB(cubCaptionDir,cubBoundingBoxFile,cubTrainTestSplit,cubFileMappings,cubClasses,pickleDir)
+    #prepData.setupCUB(cubCaptionDir,cubBoundingBoxFile,cubTrainTestSplit,cubFileMappings,cubClasses,pickleDir)
 
     # grabbing dataset
-    batchSize = 4
-    # setting none for image transforms, since we do not want to apply normalization techniques at test time
-    cubDataset = prepData.imageCaptionDataset(cubCaptionDir, cubImageDir, pickleDir,10, None, 'test', 18)
+    # using a higher batch size for saving generated images, since we are not training and we dont need
+    # an image encoder for this -> saving vram
+    batchSize = 30
+
+    # pytorch dataset object
+    cubDataset = prepData.imageCaptionDataset(cubCaptionDir, cubImageDir, pickleDir,10, transform, 'test', 18)
 
     cubDataLoader = DataLoader(cubDataset,batch_size=batchSize, drop_last=True, shuffle=True, num_workers=3)
 
@@ -124,6 +190,7 @@ if __name__ == '__main__':
     z_shape = (64,100)
     in_dim = (batchSize, 256, 256, 3)
     generator = Generator(z_shape=z_shape).to(device)
+
     #discriminator = Discriminator(in_dim).to(device)
     #discriminator = Discriminator().to(device)
 
@@ -135,7 +202,8 @@ if __name__ == '__main__':
     text_encoder = bilstmEncoder(len(cubDataset.indexToWord), 300, 0.5, 256, 18)
 
     # loading pre trained parameters of the text_encoder for cub
-    cubTextEncoderPath = "../CubDamsm/text_encoder200.pth"
+    cubTextEncoderPath = os.path.join(os.getcwd(),'CubDamsm','text_encoder200.pth')
+    #cubTextEncoderPath = "../CubDamsm/text_encoder200.pth"
     textEncoderState = torch.load(cubTextEncoderPath, map_location=lambda storage, loc: storage)
     modifiedState = OrderedDict()
     for key, values in textEncoderState.items():
@@ -149,6 +217,10 @@ if __name__ == '__main__':
 
     # loading saved model params to the text encoder
     text_encoder.load_state_dict(modifiedState)
+
+    # freeing references
+    del textEncoderState
+    del modifiedState
 
     # sending text encoder to gpu
     text_encoder.to(device)
@@ -223,11 +295,10 @@ if __name__ == '__main__':
     '''
 
     # loading training status for cub
-    trainFile = 'cubTrainingCheckpoint.pth'
-
-    if (os.path.isfile(os.path.join(os.getcwd(),trainFile))):
+    trainFile = os.path.join(os.getcwd(),'DatasetPreparation','WithDAMSMTrainingCheckpoint.pth')
+    if (os.path.isfile(trainFile)):
         print('loaded state!')
-        checkpoint = torch.load('cubTrainingCheckpoint.pth')
+        checkpoint = torch.load(trainFile)
 
         generator.load_state_dict(checkpoint['generator_state_dict'])
         #discriminator.load_state_dict(checkpoint['discriminator_state_dict'])
@@ -236,13 +307,16 @@ if __name__ == '__main__':
         generatorLoss = checkpoint['gLoss']
         discriminatorLoss = checkpoint['dLoss']
         numEpoch = checkpoint['epoch']
+        # everything is loaded, we can free some memory references
+        del checkpoint
+
 
     discriminatorLoss = None
     generatorLoss = None
 
-    # training loop
+    # testing loop
     # saving state every 10 epochs
-    test(cubDataLoader, generator, text_encoder,  device,  batchSize)
+    test(cubDataLoader, cubDataset.indexToWord, generator, text_encoder,  device,  batchSize)
 
 
 
